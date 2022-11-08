fb69fe34911118fd88fbafbd809d8cd7c5b64d3c
Jackey Cao <jackeycao128@hotmail.com>
2022-11-07 23:53:53 -0500

    Pulled more items from API.

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
-function getFunction() {
-  let selectedValue = document.getElementById("movies").value;
-  console.log(selectedValue);
-}
+function selectFunction() {
+  for (let i = 0; i < 10; i++) {
+    if (document.getElementById("movies").selectedIndex == i) {
+      return document.getElementById("movies").value
+    }
+  }
+};
 
 button.addEventListener('click', async () => {
-
   let response = axios.get("https://api.themoviedb.org/3/search/movie", {
     params: {
       api_key: "e8016904e176c4cc2f25acfd19077f5c",
       include_adult: "false",
-      query: getFunction(),
+      query: selectFunction(),
     }
   });
   response = response.then((moviesData) => {
     for (let movie of moviesData.data.results) {
       axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
         params: {
-          api_key: "779ebe30f392f779f18a739e5df2f414",
+          api_key: "e8016904e176c4cc2f25acfd19077f5c",
           append_to_response: "videos",
         }
       }).then((movieData) => {
-        const info = document.getElementById('info');
-        const img = document.getElementById('img');
-        const p = document.getElementById('p');
-        const iframe = document.getElementById('iframe');
 
-        const trailers = movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
-        iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
-        img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        p.innerHTML = `${movie.title} -- ${movie.release_date} -- ${movie.popularity}`;
-        
+        let option = movieData.data
 
+        const display = document.getElementById('display');
+        display.removeAttribute('hidden')
         
-        document.body.append(p);
-        document.body.append(img);
-        document.body.append(iframe);
+        const trailers = option.videos.results.filter((trailer) => trailer.type === "Trailer");
+        video.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
+        cover.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        let genres = "";
+        option.genres.forEach(element => {
+          genres += element.name + " "
+        });
+
+        info.innerHTML = `Title: ${option.title} <br><br> 
+        Genre: ${genres} <br><br> 
+        Language: ${option.original_language} <br>
+        Runtime (Minutes): ${option.runtime} <br>
+        Release Date: ${option.release_date} <br> 
+        Popularity: ${option.popularity} <br> 
+        Revenue: $${option.revenue} <br> 
+        Vote Average: ${option.vote_average} <br> 
+        Vote Count: ${option.vote_count}`;
+        overviewLabel.innerHTML = "Preview";
+        overviewText.innerHTML = `${option.overview}`;
       });
     }
   });
-})
+});
